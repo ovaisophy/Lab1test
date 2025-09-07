17fc60c362bb80889fcc16be5194a0da8428475c
ovais <ovaisophy@gmail.com>
2025-09-07 14:20:40 +0300

    hh

--- test.cs
+++ test.cs
@@ -6,5 +6,11 @@ class Program
     static void Main()
     {
         Console.WriteLine("Hello, World!");
+        
+        Console.WriteLine("Hello, World!");
+        
+        Console.WriteLine("Hello, World!");
+        
+        Console.WriteLine("Hello, World!");
     }
 }
